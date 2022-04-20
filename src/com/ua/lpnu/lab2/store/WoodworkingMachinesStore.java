3b456a8183b3c604e718a46bdfe19de8592d374f
maks morochenets <maksmorroo@gmail.com>
2022-04-21 00:11:15 +0300

    adding code files for lab3

--- src/com/ua/lpnu/lab2/store/WoodworkingMachinesStore.java
+++ src/com/ua/lpnu/lab2/store/WoodworkingMachinesStore.java
@@ -2,29 +2,68 @@ package com.ua.lpnu.lab2.store;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Comparator;
 
 import com.ua.lpnu.lab2.enums.TypeOfWork;
 import com.ua.lpnu.lab2.machines.*;
 
 public class WoodworkingMachinesStore {
 
-    private List<WoodworkingMachine> store = new ArrayList<>();
-    public WoodworkingMachinesStore() {}
-    public WoodworkingMachinesStore(List<WoodworkingMachine> store) {
-        this.store = store;
+
+    private List<WoodworkingMachine> storeMachines = new ArrayList<>();
+    private List<WoodworkingMachine> searchResult;
+
+    public WoodworkingMachinesStore(List<WoodworkingMachine> storeMachines) {
+            this.storeMachines = storeMachines;
+    }
+
+    public WoodworkingMachinesStore() {
+
     }
 
     public void addMachineToStore(WoodworkingMachine machine) {
-        store.add(machine);
-    }
-    public WoodworkingMachine findMachineByBrandAndType (TypeOfWork type, String manufacturerName) {
-        return null;
-    }
-    public List<WoodworkingMachine> findByPower () {
-        return null;
-    }
-    public List<WoodworkingMachine> findByVolumePerHour () {
-        return null;
+            storeMachines.add(machine);
     }
 
+    public List<WoodworkingMachine> findMachineByBrandAndType (TypeOfWork type, String manufacturerName) {
+        searchResult = storeMachines.stream()
+                .filter(machine->machine.getTypeOfWork().equals(type))
+                .filter(machine->machine.getManufacturerName().equals(manufacturerName))
+                .collect(Collectors.toList());
+        return searchResult;
+    }
+
+    public List<WoodworkingMachine> sortByPower (boolean ascending) {
+        if (ascending) {
+            searchResult = searchResult.stream()
+                    .sorted(Comparator.comparing(WoodworkingMachine::getPowerInWatts))
+                    .collect(Collectors.toList());
+        } else {
+            searchResult = searchResult.stream()
+                    .sorted(Comparator.comparing(WoodworkingMachine::getPowerInWatts).reversed())
+                    .collect(Collectors.toList());
+        }
+        return searchResult;
+    }
+
+    public List<WoodworkingMachine> sortByVolumePerHour (boolean ascending) {
+        if (ascending) {
+            searchResult = searchResult.stream()
+                    .sorted(Comparator.comparing(WoodworkingMachine::getWoodVolumePerHour))
+                    .collect(Collectors.toList());
+        } else {
+            searchResult = searchResult.stream()
+                    .sorted(Comparator.comparing(WoodworkingMachine::getWoodVolumePerHour).reversed())
+                    .collect(Collectors.toList());
+        }
+        return searchResult;
+    }
+
+
 }
+
+
+
+
+
